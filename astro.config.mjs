cf2e5c9ebbc9405991aedc3c006149a3c927e3d0
Fuzzy Bear <28299972+aFuzzyBear@users.noreply.github.com>
2025-07-26 16:35:05 +0100

    fix: add vite preview configuration with allowed hosts to astro.config.mjs
    Signed-off-by: Fuzzy Bear <28299972+aFuzzyBear@users.noreply.github.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,10 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     output: 'static',
-    site: "https://hello.deploy.afuzzy.cloud"
+    site: "https://hello.deploy.afuzzy.cloud",
+    vite: {
+        preview: {
+            allowedHosts: ['hello.deploy.afuzzy.cloud', 'localhost'],
+        }
+    }
 });
